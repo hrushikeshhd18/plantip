3891e0734a45c9e98fb78955d9a4ea0438907ba9
hrushikeshdesai <hrutikdesai1@gmail.com>
2022-02-02 14:18:50 +0530

    Initial commit

---
+++ app/src/main/java/com/hdgroup/plantip/Intro/LanguageActivity.java
@@ -0,0 +1,99 @@
+package com.hdgroup.plantip.Intro;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.hdgroup.plantip.Helper.MyContextWrapper;
+import com.hdgroup.plantip.MainActivity;
+import com.hdgroup.plantip.R;
+
+
+
+public class LanguageActivity extends AppCompatActivity {
+
+    TextView textView;
+    Button E1, M1;
+    private String LANG_CURRENT = "en";
+    Context context;
+    Resources resources;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_language);
+        textView = findViewById(R.id.text1);
+        E1 = findViewById(R.id.Englishbtn);
+        M1 = findViewById(R.id.Marathibtn);
+
+
+        E1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+//                context = LocaleHelper.setLocale(LanguageActivity.this, "en");
+//                resources = context.getResources();
+//                textView.setText(resources.getString(R.string.hello_world));
+
+
+                if (LANG_CURRENT.equals("mr")) {
+                    changeLang(LanguageActivity.this, "en");
+                } else {
+                    changeLang(LanguageActivity.this, "mr");
+                }
+                finish();
+                startActivity(new Intent(LanguageActivity.this, MainActivity.class));
+            }
+        });
+
+
+        M1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if (LANG_CURRENT.equals("en")) {
+                    changeLang(LanguageActivity.this, "mr");
+                } else {
+                    changeLang(LanguageActivity.this, "en");
+                }
+                finish();
+                startActivity(new Intent(LanguageActivity.this, MainActivity.class));
+            }
+        });
+    }
+
+
+
+
+
+
+
+
+    public void changeLang(Context context, String lang) {
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
+        SharedPreferences.Editor editor = preferences.edit();
+        editor.putString("Language", lang);
+        editor.apply();
+    }
+
+    @Override
+    protected void attachBaseContext(Context newBase) {
+
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(newBase);
+        LANG_CURRENT = preferences.getString("Language", "en");
+
+        super.attachBaseContext(MyContextWrapper.wrap(newBase, LANG_CURRENT));
+    }
+
+}
+
+
+
+
---
+++ app/src/main/java/com/hdgroup/plantip/Intro/Splashscreen.java
@@ -0,0 +1,112 @@
+package com.hdgroup.plantip.Intro;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+import androidx.viewpager.widget.ViewPager;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Adapter;
+import android.widget.ImageView;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.hdgroup.plantip.MainActivity;
+import com.hdgroup.plantip.R;
+
+public class Splashscreen extends AppCompatActivity {
+
+    ImageView logo,background;
+    LottieAnimationView lottieAnimationView;
+
+    private static final int slidesno =3;
+
+    private ViewPager viewPager;
+    private ScreenSliderAdapter sliderAdapter;
+    Animation animation;
+
+    private static int SPLASH_TIME_OUT = 6000;
+    SharedPreferences sharedPreferences;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splashscreen);
+
+        logo = findViewById(R.id.logo);
+        background = findViewById(R.id.bgimage);
+        lottieAnimationView = findViewById(R.id.lottieani);
+
+        viewPager = findViewById(R.id.pager);
+        sliderAdapter = new ScreenSliderAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(sliderAdapter);
+
+        animation = AnimationUtils.loadAnimation(this,R.anim.onboardanim);
+        viewPager.startAnimation(animation);
+
+        background.animate().translationY(-1600).setDuration(4000).setStartDelay(4000);
+        logo.animate().translationY(400).setDuration(2000).setStartDelay(3000);
+        lottieAnimationView.animate().translationY(300).setDuration(2000).setStartDelay(3000);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                sharedPreferences = getSharedPreferences("sharedpref",MODE_PRIVATE);
+                boolean isFirestTime = sharedPreferences.getBoolean("firsttime",true);
+
+                if (isFirestTime){
+                    //intent
+                    SharedPreferences.Editor editor = sharedPreferences.edit();
+                    editor.putBoolean("firsttime",false);
+                    editor.commit();
+
+                }
+                else {
+                    Intent intent = new Intent(Splashscreen.this, LanguageActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        },SPLASH_TIME_OUT);
+    }
+
+    private class ScreenSliderAdapter extends FragmentStatePagerAdapter{
+
+        public ScreenSliderAdapter(@NonNull FragmentManager fm) {
+            super(fm);
+        }
+
+        @NonNull
+        @Override
+        public Fragment getItem(int position) {
+            switch (position){
+                case 0:
+                    OnBoardingFragment1 tab1 = new OnBoardingFragment1();
+                    return tab1;
+                case 1:
+                    OnBoardingFragment2 tab2 = new OnBoardingFragment2();
+                    return tab2;
+                case 2:
+                    OnBoardingFragment3 tab3 = new OnBoardingFragment3();
+                    return tab3;
+
+            }
+            return null;
+        }
+
+        @Override
+        public int getCount() {
+
+            return slidesno;
+        }
+    }
+}
